323c2494171b0092de9e608aaa9c22f214f46260
[zapjelly] <[pak.cindy@gmail.com]>
2017-12-01 10:53:39 +1100

    add props to check change

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,11 +5,18 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Checkbox from '../../src'
 
 let Demo = React.createClass({
+  onChange(field){
+    console.log("in OnChange")
+    return (value) => {
+      console.log(field, value)
+    }
+  },
+
   render() {
     return(
-    <MuiThemeProvider>
-      <Checkbox label="I am a checkbox" />
-    </MuiThemeProvider>
+      <MuiThemeProvider>
+        <Checkbox label="I am a checkbox" onCheck={this.onChange("checkbox")}/>
+      </MuiThemeProvider>
     )
   }
 })
